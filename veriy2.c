95c5362c07d66eb333187915a273c40fa56bb231
Sevgi Nur Öksüz <90787721+sevginuroksuz@users.noreply.github.com>
2022-11-15 00:21:05 +0300

    commit2

--- veriy2.c
+++ veriy2.c
@@ -1,45 +1,48 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-struct dugum{
+// Tek yonlu dogrusal bagli liste sona eleman ekleme 
+
+struct dugum{      // struct yapisi
 
     int veri;
     struct dugum * gosterici;
 
 };
 
-   struct dugum * bas=NULL;
-   struct dugum * gecici=NULL;
+   struct dugum * bas=NULL;      // bas dugum
+   struct dugum * gecici=NULL;    // degisikilikleri yapmamizi saglayacak dugum
 
 
-   void sonaEkle(int sayi){
+   void sonaEkle(int sayi){    // dugum eklemeyi saglayacak fonksiyon
 
-    struct dugum * son;
+    struct dugum * son;                          
     son=(struct dugum*) malloc (sizeof(struct dugum));
     son->veri=sayi;
-    son->gosterici==NULL;
+    son->gosterici=NULL;
     
-    if(bas==NULL){   
+    if(bas==NULL){    
         bas=son;    // listede dugum yoksa eklenen dugum ilk dugume esittir
 
     }
     else{
         gecici=bas;    // listede baska elemanlar varsa gecici dugum yardimiyla liste son elemanina gelene kadar sonsuz dongude gezilir.
-        while(gecici->gosterici==NULL){
-            gecici=gecici->gosterici;
+        while(gecici->gosterici!=NULL){
+            gecici=gecici->gosterici;  
         }
-        gecici->gosterici=son;
+        gecici->gosterici=son;  // son dugumu buldugunda eklenecek dugumu gecici dugume isaret ettiriyoruz
 
     }
    }
 
-    void yazdir(){
-        gecici=bas;
-        while(gecici->gosterici!=NULL){
+    void yazdir(){ // ilk dugumden itibaren son dugumde dahil NULL gorene kadar yazdiran fonksiyonu yazdik
+        gecici=bas;       
+        while(gecici->gosterici!=NULL){    
         printf(" %d ->", gecici->veri);
         gecici=gecici->gosterici;
 
         }
+        printf(" %d ", gecici->veri);  // son dugum yazdirilmasi icin (son dugum NULL'i isaret ettigi icin while dongusunde yazdirilmaz)
         
 
     }
@@ -48,10 +51,8 @@ struct dugum{
 int main(){
 
     int i=0;
-    int Sayi;
-    
-
-    while(i>=3){
+    while(i!=7){  // 7 elemanli tek yonlu dogrusal bagli liste olusturulup yazdirilir
+        int Sayi;
         printf("Bir sayi giriniz: \n");
         scanf("%d",&Sayi);
         sonaEkle(Sayi);
@@ -59,8 +60,6 @@ int main(){
     }
     
        yazdir();
-
-
 
     return 0;
 }
